bb74f19cde0866691867bc91f555447608b316da
Maike Schröer <maikeschroeer@gmx.de>
2023-06-02 21:15:51 +0200

    create pets and user seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :pets
+  has_many :pets, dependent: :destroy
   has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
