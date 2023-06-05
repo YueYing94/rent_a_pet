377263eeee5aeba73447383e65b7c924e89aec2d
yue ying <sarahyingyue1994@gmail.com>
2023-06-05 19:35:40 +0200

    if not logged in, show all pets. if logged in, show pets that doesnt belong to current user. also made the show page available for not logged in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only:[:home]
   def home
-    @pets = Pet.all
+    if current_user.present?
+      all_pet = Pet.all
+      @pets = []
+      all_pet.each do |pet|
+        @pets << pet if pet.user_id != current_user.id
+        end
+      @pets
+    else
+      @pets = Pet.all
+    end
   end
 end
