1369e618e8d9d01c7679bf0f801ee4ba952bbcfd
yue ying <sarahyingyue1994@gmail.com>
2023-06-08 21:00:47 +0200

    need to fix conflicts with master

--- app/javascript/controllers/add_map_controller.js
+++ app/javascript/controllers/add_map_controller.js
@@ -9,27 +9,10 @@ export default class extends Controller {
   }
 
   change(event) {
-    // this.buttonTarget.innerHTML = `Show Map <i class="fa-solid fa-map-location-dot"></i>`;
-    // if (this.event.currentTarget.value === "list") {
-    //   this.buttonTarget.innerHTML = `Show List <i class="fa-solid fa-list-ul"></i>`;
-    // } else {
-    //   this.event.currentTarget.value = "map"
-    // }
     this.buttonmapTarget.classList.toggle("d-none");
     this.buttonlistTarget.classList.toggle("d-none");
     this.listTarget.classList.toggle("d-none");
     this.mapTarget.classList.toggle("d-none");
     console.log(this.listTarget);
-    // if (
-    //   this.buttonTarget.innerHTML === `Show Map <i class="fa-solid fa-map-location-dot"></i>`
-    // ) {
-    //   this.buttonTarget.innerHTML = `Show List <i class="fa-solid fa-list-ul"></i>`;
-    //   this.listTarget.classList.add("d-none");
-    //   this.mapTarget.classList.remove("d-none");
-    // } else {
-    //   this.buttonTarget.innerHTML = `Show Map <i class="fa-solid fa-map-location-dot"></i>`;
-    //   this.listTarget.classList.remove("d-none");
-    //   this.mapTarget.classList.add("d-none");
-    // }
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/profile", to: "users#profile"
+  get "/users/:id", to: "users#show"
   resources :pets do
     resources :bookings, only:[:create]
     collection do
