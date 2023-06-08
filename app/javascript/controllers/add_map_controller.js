43147a86910587306fc1da1a6e7a960b5d739f8e
yue ying <sarahyingyue1994@gmail.com>
2023-06-08 19:22:30 +0200

    change javascript

--- app/javascript/controllers/add_map_controller.js
+++ app/javascript/controllers/add_map_controller.js
@@ -2,25 +2,34 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="add-map"
 export default class extends Controller {
-  static targets = ["button", "list", "map"];
-  
+  static targets = ["button", "list", "map", "buttonmap", "buttonlist"];
+
   connect() {
-    document.addEventListener("DOMContentLoaded", () => {
-      console.log("hello add map controller");
-    });
+    console.log("hello add map controller");
   }
-  change() {
-    if (
-      this.buttonTarget.innerHTML ===
-      `Show Map <i class="fa-solid fa-map-location-dot"></i>`
-    ) {
-      this.buttonTarget.innerHTML = `Show List <i class="fa-solid fa-list-ul"></i>`;
-      this.listTarget.classList.add("d-none");
-      this.mapTarget.classList.remove("d-none");
-    } else {
-      this.buttonTarget.innerHTML = `Show Map <i class="fa-solid fa-map-location-dot"></i>`;
-      this.listTarget.classList.remove("d-none");
-      this.mapTarget.classList.add("d-none");
-    }
+
+  change(event) {
+    // this.buttonTarget.innerHTML = `Show Map <i class="fa-solid fa-map-location-dot"></i>`;
+    // if (this.event.currentTarget.value === "list") {
+    //   this.buttonTarget.innerHTML = `Show List <i class="fa-solid fa-list-ul"></i>`;
+    // } else {
+    //   this.event.currentTarget.value = "map"
+    // }
+    this.buttonmapTarget.classList.toggle("d-none");
+    this.buttonlistTarget.classList.toggle("d-none");
+    this.listTarget.classList.toggle("d-none");
+    this.mapTarget.classList.toggle("d-none");
+    console.log(this.listTarget);
+    // if (
+    //   this.buttonTarget.innerHTML === `Show Map <i class="fa-solid fa-map-location-dot"></i>`
+    // ) {
+    //   this.buttonTarget.innerHTML = `Show List <i class="fa-solid fa-list-ul"></i>`;
+    //   this.listTarget.classList.add("d-none");
+    //   this.mapTarget.classList.remove("d-none");
+    // } else {
+    //   this.buttonTarget.innerHTML = `Show Map <i class="fa-solid fa-map-location-dot"></i>`;
+    //   this.listTarget.classList.remove("d-none");
+    //   this.mapTarget.classList.add("d-none");
+    // }
   }
 }
