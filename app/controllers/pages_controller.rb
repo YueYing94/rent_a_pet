b2f841f1c950659ca05f59d157f7b5b952b84035
yue ying <sarahyingyue1994@gmail.com>
2023-06-09 21:58:06 +0200

    check other branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,8 @@ class PagesController < ApplicationController
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
-        lng: user.longitude
+        lng: user.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {user: user})
       }
     end
 
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,18 +1,62 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 export default class extends Controller {
+  static targets = [
+    "button",
+    "list",
+    "map",
+    "buttonmap",
+    "buttonlist",
+    "container",
+  ];
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+  };
 
   connect() {
-    console.log(this.apiKeyValue);
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
-    })
+      container: this.containerTarget,
+      style: "mapbox://styles/mapbox/streets-v10",
+    });
+
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+    this.map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  }
+
+  change(event) {
+    console.log("click");
+    this.buttonmapTarget.classList.toggle("d-none");
+    this.buttonlistTarget.classList.toggle("d-none");
+    this.listTarget.classList.toggle("d-none");
+    this.mapTarget.classList.toggle("d-none");
+    this.map.resize();
   }
 }
