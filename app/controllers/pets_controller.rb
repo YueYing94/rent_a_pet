fbc2eea948fcc67cfab5214a8285f588b23fada0
max delaurey <mvxdelaurey@gmail.com>
2023-06-10 07:23:18 +0100

    fixed typos

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,7 +4,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet_review = PetReview.new
     @booking = Booking.new
-    @markers = @pet.user.geocode.map do |flat|
+    @markers = @pet.user.geocode.map do |pet|
       {
         lat: @pet.user.latitude,
         lng: @pet.user.longitude
